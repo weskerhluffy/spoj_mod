7e146a8f9653afecda6a5f91dbc5bcce2e4392dd
ernesto.alvarado <ernesto.alvarado@adroll.com>
2019-08-30 20:27:00 -0500

    Initial Commit

---
+++ spoj_mod/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  spoj_mod
+//
+//  Created by ernesto alvarado on 30/08/19.
+//  Copyright © 2019 ernesto alvarado. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
